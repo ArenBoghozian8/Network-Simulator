ef94eaef4f06df2009e3ba7d35614ad916ccaf57
Aren Boghozian <aren.mboghozian@gmail.com>
2017-11-12 22:11:55 -0800

    Add files via upload
    DRR runs with our new BASE CLASS

--- deficit-round-robin/model/deficit-round-robin.cc
+++ deficit-round-robin/model/deficit-round-robin.cc
@@ -24,139 +24,107 @@ namespace ns3 {
 NS_OBJECT_ENSURE_REGISTERED(DeficitRoundRobin);
 
 TypeId DeficitRoundRobin::GetTypeId(void) {
-	static TypeId tid =
-			TypeId("ns3::DeficitRoundRobinQueue").SetParent<Queue>().AddConstructor<
-			        DeficitRoundRobin>()
-			.AddAttribute("SecondQueueMaxPackets",
-					"The maximum number of packets accepted by the second weighted queue.",
-					UintegerValue(100),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_secondMaxPackets),
-					MakeUintegerChecker<uint32_t>())
+	static TypeId tid = TypeId("ns3::DeficitRoundRobin")
+     .SetParent<BaseClass> ()
+     .AddConstructor<DeficitRoundRobin> ()
+     .AddAttribute ("DRRMode",
+                    "Whether to use bytes (see MaxBytes) or packets (see MaxPackets) as the maximum queue size metric.",
+                   EnumValue (QUEUE_MODE_PACKETS),
+                   MakeEnumAccessor (&DeficitRoundRobin::SetMode),
+                   MakeEnumChecker (QUEUE_MODE_BYTES, "QUEUE_MODE_BYTES",
+                                    QUEUE_MODE_PACKETS, "QUEUE_MODE_PACKETS"))
+    .AddAttribute ("HighPriorityMaxPackets",
+                   "The maximum number of packets accepted by the high priority queue.",
+                   UintegerValue (100),
+                   MakeUintegerAccessor (&DeficitRoundRobin::m_highMaxPackets),
+                   MakeUintegerChecker<uint32_t> ())
 
-			.AddAttribute("FirstQueueMaxPackets",
-					"The maximum number of packets accepted by the first weighted queue.",
-					UintegerValue(100),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_firstMaxPackets),
-					MakeUintegerChecker<uint32_t>())
+    .AddAttribute ("LowPriorityMaxPackets",
+                   "The maximum number of packets accepted by the low priority queue.",
+                   UintegerValue (100),
+                   MakeUintegerAccessor (&DeficitRoundRobin::m_lowMaxPackets),
+                   MakeUintegerChecker<uint32_t> ())
 
-			.AddAttribute("SecondQueueMaxBytes",
-					"The maximum number of bytes accepted by the second weighted queue.",
-					UintegerValue(100 * 65535),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_secondMaxBytes),
-					MakeUintegerChecker<uint32_t>())
+    .AddAttribute ("HighPriorityMaxBytes",
+                   "The maximum number of bytes accepted by the high priority queue.",
+                   UintegerValue (100 * 65535),
+                   MakeUintegerAccessor (&DeficitRoundRobin::m_highMaxBytes),
+                   MakeUintegerChecker<uint32_t> ())
 
-			.AddAttribute("FirstQueueMaxBytes",
-					"The maximum number of bytes accepted by the first weighted queue.",
-					UintegerValue(100 * 65535),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_firstMaxBytes),
-					MakeUintegerChecker<uint32_t>())
+    .AddAttribute ("LowPriorityMaxBytes",
+                   "The maximum number of bytes accepted by the low priority queue.",
+                   UintegerValue (100 * 65535),
+                   MakeUintegerAccessor (&DeficitRoundRobin::m_lowMaxBytes),
+                   MakeUintegerChecker<uint32_t> ())
 
-			.AddAttribute("SecondQueuePort",
-					"The destination port number for second queue traffic.",
-					UintegerValue(3000),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_secondQueuePort),
-					MakeUintegerChecker<uint32_t>())
-			.AddAttribute("TotalExpectedBytes",
-					"Total expected bytes passed before end of simulation - used for showing progression percentage",
-					UintegerValue(0),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_totalExpectedBytes),
-					MakeUintegerChecker<uint64_t>())
-
-			.AddAttribute("FirstMeanPacketSize",
-					"The mean packet size of first queue",
-					UintegerValue(1),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_firstMeanPacketSize),
-					MakeUintegerChecker<uint32_t>())
-
-			.AddAttribute("SecondMeanPacketSize",
-					"The mean packet size of second queue",
-					UintegerValue(1),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_secondMeanPacketSize),
-					MakeUintegerChecker<uint32_t>())
-                        .AddAttribute("QuantumSize",
-					"The packet size permitted for each queue",
-					UintegerValue(1),
-					MakeUintegerAccessor(&DeficitRoundRobin::m_quantum_size),
-					MakeUintegerChecker<uint32_t>());
-
+    .AddAttribute ("HighPriorityPort",
+                   "The destination port number for high priority traffic.",
+                   UintegerValue (3000),
+                   MakeUintegerAccessor (&DeficitRoundRobin::m_priorityPort),
+                   MakeUintegerChecker<uint32_t> ())
+      .AddAttribute("QuantumSize",
+                  "The packet size permitted for each queue",
+                  UintegerValue(1),
+                  MakeUintegerAccessor(&DeficitRoundRobin::quantumSize),
+                  MakeUintegerChecker<uint32_t>());
 	return tid;
 }
 
-DeficitRoundRobin::DeficitRoundRobin() :
-		Queue(), m_firstQueue(), m_bytesInFirstQueue(0), m_secondQueue(), m_bytesInSecondQueue(0)
-			    {
-	NS_LOG_FUNCTION_NOARGS ();
+DeficitRoundRobin::DeficitRoundRobin(): BaseClass(){
+  this->m_serve_queue2 = false;
+  this->m_highMaxPackets = 100;
+  this->m_lowMaxPackets = 100;
+  this->m_highMaxBytes = 100*65535;
+  this->m_lowMaxBytes = 100*65535;
+  this->m_priorityPort = 3000;
 
-	m_isInitialized = false;
 
-	m_passedBytes = 0;
-	m_20PercentCompleted = false;
-	m_40PercentCompleted = false;
-	m_60PercentCompleted = false;
-	m_80PercentCompleted = false;
-    m_serve_queue2 = false;
-    m_first_dc = 0;
-    m_second_dc =0;
-	max_firstQueue = 0;
-	max_secondQueue = 0;
+  q_class[0] = new TrafficClass;              //initialize high priority queue
+  q_class[1] = new TrafficClass;              //initialize low  priority queue
+  q_class[0]->setBytes(0);                    //initialize
+  q_class[1]->setBytes(0);                    //initialize
+  q_class[0]->setMaxPackets(m_highMaxPackets);//Custom settings
+  q_class[1]->setMaxPackets(m_lowMaxPackets); //Custom settings
+  q_class[0]->setMaxBytes(m_highMaxBytes);    //Custom settings
+  q_class[1]->setMaxBytes(m_lowMaxBytes);     //Custom settings
+  DestinationPortNumber * prioPN = new DestinationPortNumber(m_priorityPort); 
+
+  Filter * fil;
+  fil->set_element(prioPN);
+  q_class[0]->filters.push_back(fil);
+
+
+  NS_LOG_FUNCTION_NOARGS ();
 }
 
 DeficitRoundRobin::~DeficitRoundRobin() {
 	NS_LOG_FUNCTION_NOARGS ();
 }
 
-void DeficitRoundRobin::SetMode(DeficitRoundRobin::QueueMode mode) {
-	NS_LOG_FUNCTION(mode);
-	m_mode = mode;
+
+void
+DeficitRoundRobin::SetMode (DeficitRoundRobin::QueueMode mode)
+{
+  NS_LOG_FUNCTION (mode);
+  m_mode = mode;
 }
 
-DeficitRoundRobin::QueueMode DeficitRoundRobin::GetMode(void) {
-	NS_LOG_FUNCTION_NOARGS ();
-	return m_mode;
+DeficitRoundRobin::QueueMode
+DeficitRoundRobin::GetMode (void)
+{
+  NS_LOG_FUNCTION_NOARGS ();
+  return m_mode;
 }
 
-uint16_t DeficitRoundRobin::Classify(Ptr<Packet> p) {
-	NS_LOG_FUNCTION(this << p);
-	PppHeader ppp;
-	p->RemoveHeader(ppp);
-	Ipv4Header ip;
-	p->RemoveHeader(ip);
-        
-   
-	uint16_t weightedQueue;
 
-	uint32_t protocol = ip.GetProtocol();
-	if (protocol == 17) {
-		UdpHeader udp;
-		p->PeekHeader(udp);
 
-		if (udp.GetDestinationPort() == m_secondQueuePort) {
-			NS_LOG_INFO("classifier: second queue udp");
-			weightedQueue = 1;
-		} else {
-			NS_LOG_INFO("classifier: first queue udp");
-			weightedQueue = 0;
-		}
-	}
+uint16_t DeficitRoundRobin::classify(Ptr<QueueItem> p) {
 
-	else if (protocol == 6) {
-		TcpHeader tcp;
-		p->PeekHeader(tcp);
-		if (tcp.GetDestinationPort() == m_secondQueuePort) {
-			NS_LOG_INFO("classifier: second queue tcp");
-			weightedQueue = 1;
-		} else {
-			NS_LOG_INFO("classifier: first queue tcp");
-			weightedQueue = 0;
-		}
-
-	} else {
-		NS_LOG_INFO("\tclassifier: unrecognized transport protocol");
-		weightedQueue = 0;
-	}
-
-	p->AddHeader(ip);
-	p->AddHeader(ppp);
+   uint16_t weightedQueue;
+   if(q_class[0]->filters[0]->element[0]->match(p))
+      weightedQueue = 1;
+    else 
+     weightedQueue = 0;
 
 	return weightedQueue;
 }
@@ -166,35 +134,28 @@ uint16_t DeficitRoundRobin::Classify(Ptr<Packet> p) {
 bool DeficitRoundRobin::DoEnqueue(Ptr<QueueItem> p) {
 	NS_LOG_FUNCTION(this << p);
 
-	if (m_isInitialized == false)
-	{
-		m_isInitialized = true;
-	}
-
-	uint16_t weightedQueue = Classify(p->GetPacket());
+	uint16_t weightedQueue = classify(p);
 
 	// Second Queue
 	if (weightedQueue == 1) {
 		if (m_mode == QUEUE_MODE_PACKETS
-				&& (m_secondQueue.size() >= m_secondMaxPackets)) {
+				&& (q_class[0]->m_queue.size() >= q_class[0]->getMaxPackets())) {
 			NS_LOG_LOGIC("Queue full (at max packets) -- dropping pkt");
 			Drop(p->GetPacket());
 			return false;
 		}
 
 		if (m_mode == QUEUE_MODE_BYTES
-				&& (m_bytesInSecondQueue + p->GetPacket()->GetSize() >= m_secondMaxBytes)) {
+				&& (q_class[0]->getBytes() + p->GetPacket()->GetSize() >= q_class[0]->getMaxBytes())) {
 			NS_LOG_LOGIC(
 					"Queue full (packet would exceed max bytes) -- dropping pkt");
 			Drop(p->GetPacket());
 			return false;
 		}
 
-		m_bytesInSecondQueue += p->GetPacket()->GetSize();
-		m_secondQueue.push(p->GetPacket());
+		q_class[0]->setBytes(q_class[0]->getBytes() + p->GetPacket()->GetSize());
+		q_class[0]->m_queue.push(p);
 
-		NS_LOG_LOGIC("Number packets " << m_secondQueue.size ());
-		NS_LOG_LOGIC("Number bytes " << m_bytesInSecondQueue);
 
 		return true;
 	}
@@ -202,25 +163,25 @@ bool DeficitRoundRobin::DoEnqueue(Ptr<QueueItem> p) {
 	// First Queue
 	else if (weightedQueue == 0) {
 		if (m_mode == QUEUE_MODE_PACKETS
-				&& (m_firstQueue.size() >= m_firstMaxPackets)) {
+				&& (q_class[1]->m_queue.size() >= q_class[1]->getMaxPackets())) {
 			NS_LOG_LOGIC("Queue full (at max packets) -- dropping pkt");
 			Drop(p->GetPacket());
 			return false;
 		}
 
 		if (m_mode == QUEUE_MODE_BYTES
-				&& (m_bytesInFirstQueue + p->GetPacket()->GetSize() >= m_firstMaxBytes)) {
+				&& (q_class[1]->getBytes() + p->GetPacket()->GetSize() >= q_class[1]->getMaxBytes())) {
 			NS_LOG_LOGIC(
 					"Queue full (packet would exceed max bytes) -- dropping pkt");
 			Drop(p->GetPacket());
 			return false;
 		}
 
-		m_bytesInFirstQueue += p->GetPacket()->GetSize();
-		m_firstQueue.push(p->GetPacket());
+    q_class[1]->setBytes(q_class[1]->getBytes() + p->GetPacket()->GetSize());
+		q_class[1]->m_queue.push(p);
 
-		NS_LOG_LOGIC("Number packets " << m_firstQueue.size ());
-		NS_LOG_LOGIC("Number bytes " << m_bytesInFirstQueue);
+		//NS_LOG_LOGIC("Number packets " << m_firstQueue.size ());
+		//NS_LOG_LOGIC("Number bytes " << m_bytesInFirstQueue);
 
 		return true;
 	}
@@ -236,54 +197,61 @@ bool DeficitRoundRobin::DoEnqueue(Ptr<QueueItem> p) {
 Ptr<QueueItem> DeficitRoundRobin::DoDequeue(void) {
 	NS_LOG_FUNCTION(this);
 
-    if(!m_firstQueue.empty() || !m_secondQueue.empty()){
-        if (!m_firstQueue.empty() && !m_serve_queue2) { //Check == false
-                m_first_dc += m_quantum_size;
-	            Ptr<Packet> packet1 = m_firstQueue.front();
-
-               if(packet1->GetSize() < m_quantum_size){
-                        m_firstQueue.pop();
-                        m_first_dc -= packet1->GetSize();
+    if(!q_class[1]->m_queue.empty() || !q_class[0]->m_queue.empty()){
+        if (!q_class[1]->m_queue.empty() && !m_serve_queue2) { //Check == false
+                m_first_dc += quantumSize;
+                Ptr<QueueItem> packet1 = q_class[1]->m_queue.front();
+                NS_LOG_LOGIC("-------------------Queue1 is being served-----------------------");
+                NS_LOG_LOGIC("Number bytes in first queue1 " << q_class[1]->getBytes());
+                NS_LOG_LOGIC("Defecit Counter Queue1 before " << m_first_dc);
+               if(packet1->GetPacket()->GetSize() < m_first_dc){
+                        q_class[1]->setBytes(q_class[1]->getBytes() - packet1->GetPacket()->GetSize());
+                        q_class[1]->m_queue.pop();
+                        m_first_dc -= packet1->GetPacket()->GetSize();
+                        
                 }
-	            m_bytesInFirstQueue -= packet1->GetSize();
                 
-                if(m_firstQueue.empty()){
-                        m_first_dc = 0;
+                if(q_class[1]->m_queue.empty()){
+                	m_first_dc = 0;
                 }
     
-	            NS_LOG_LOGIC("Popped " << packet1);
-	            NS_LOG_LOGIC("Number packets " << m_firstQueue.size ());
-	            NS_LOG_LOGIC("Number bytes " << m_bytesInFirstQueue);
-                NS_LOG_LOGIC("Queue1 is being served");
+                
+                NS_LOG_LOGIC("Number packets " << q_class[1]->m_queue.size ());
+                NS_LOG_LOGIC("Packet SIze " << packet1->GetPacket()->GetSize());
+                NS_LOG_LOGIC("Number bytes in first queue2 " << q_class[1]->getBytes());
+                
+                
 
                 m_serve_queue2 = true;
-                NS_LOG_LOGIC("Defecit Counter Queue1" << m_first_dc);
-	            return new QueueItem(packet1);
+                NS_LOG_LOGIC("Defecit Counter Queue1 After " << m_first_dc);
+                return new QueueItem(packet1->GetPacket());
         }
 
-        else if(!m_secondQueue.empty() && m_serve_queue2) { //check == true
-                m_second_dc += m_quantum_size;
-	            Ptr<Packet> packet2 = m_secondQueue.front();
-                
-                if(packet2->GetSize() < m_quantum_size){
-                        m_secondQueue.pop();
-                        m_second_dc -= packet2->GetSize();
+        else if(!q_class[0]->m_queue.empty() && m_serve_queue2) { //check == true
+                m_second_dc += quantumSize;
+                Ptr<QueueItem> packet2 = q_class[0]->m_queue.front();
+                NS_LOG_LOGIC("------------------Queue2 is being served-------------------------");                
+                NS_LOG_LOGIC("Defecit Counter Queue2 before" << m_second_dc);                
+                if(packet2->GetPacket()->GetSize() < m_second_dc){
+                        q_class[0]->setBytes(q_class[0]->getBytes() - packet2->GetPacket()->GetSize());
+                        q_class[0]->m_queue.pop();
+                        m_second_dc -= packet2->GetPacket()->GetSize();
+                        
                 }
 
-	            m_bytesInSecondQueue -= packet2->GetSize();
                 
-                if(m_secondQueue.empty()){
-                        m_second_dc = 0;
-                }
+                //if(m_secondQueue.empty()){
+                  //      m_second_dc = 0;
+                //}
 
-	            NS_LOG_LOGIC("Popped " << packet2);
-	            NS_LOG_LOGIC("Number packets " << m_secondQueue.size ());
-	            NS_LOG_LOGIC("Number bytes " << m_bytesInSecondQueue);
-                NS_LOG_LOGIC("Queue2 is being served");
+                NS_LOG_LOGIC("Number packets " << q_class[0]->m_queue.size ());
+                NS_LOG_LOGIC("Packet SIZE " << packet2->GetPacket()->GetSize()); 
+                NS_LOG_LOGIC("Number bytes in second Queue " << q_class[0]->getBytes());
+                               
 
                 m_serve_queue2=false;
-                NS_LOG_LOGIC("Defecit Counter Queue2" << m_second_dc);
-	            return new QueueItem(packet2);
+                NS_LOG_LOGIC("Defecit Counter Queue2 After" << m_second_dc);
+                return new QueueItem(packet2->GetPacket());
         }
         else
                 return NULL;
@@ -305,18 +273,18 @@ Ptr<const QueueItem> DeficitRoundRobin::DoPeek(void) const {
 
 	if (queueToBeServed == 0) {
 
-		Ptr<Packet> pack1 = m_firstQueue.front ();
-		NS_LOG_LOGIC("Number packets " << m_firstQueue.size ());
-		NS_LOG_LOGIC("Number bytes " << m_bytesInFirstQueue);
+		Ptr<Packet> pack1 = q_class[1]->m_queue.front()->GetPacket();
+		NS_LOG_LOGIC("Number packets " << q_class[1]->m_queue.size ());
+		NS_LOG_LOGIC("Number bytes " << q_class[1]->getBytes());
                 
 		return new QueueItem(pack1);
 	}
 
 	else if (queueToBeServed == 1) {
 
-		Ptr<Packet> pack2 = m_secondQueue.front ();
-		NS_LOG_LOGIC("Number packets " << m_secondQueue.size ());
-		NS_LOG_LOGIC("Number bytes " << m_bytesInSecondQueue);
+		Ptr<Packet> pack2 = q_class[0]->m_queue.front()->GetPacket();
+		NS_LOG_LOGIC("Number packets " << q_class[0]->m_queue.size ());
+		NS_LOG_LOGIC("Number bytes " << q_class[1]->getBytes());
 
 		return new QueueItem(pack2);
 	}
@@ -330,33 +298,39 @@ Ptr<const QueueItem> DeficitRoundRobin::DoPeek(void) const {
 
 int32_t DeficitRoundRobin::GetQueueToBePeeked() const
 {
-	if (m_firstRemainingPacketsToBeServed > 0)
+	if (q_class[1]->getPackets() > 0)
 	{
-		if (!m_firstQueue.empty())
+		if (!q_class[1]->m_queue.empty())
 		{
 			return 0;
 		}
 	}
 
-	if (m_secondRemainingPacketsToBeServed > 0)
+	if (q_class[0]->getPackets() > 0)
 	{
-		if (!m_secondQueue.empty())
+		if (!q_class[0]->m_queue.empty())
 		{
 			return 1;
 		}
 	}
 
-	if (!m_firstQueue.empty())
+	if (!q_class[1]->m_queue.empty())
 	{
 		return 0;
 	}
 
-	if (!m_secondQueue.empty())
+	if (!q_class[0]->m_queue.empty())
 	{
 		return 1;
 	}
 
 	return -1;
+}
+
+
+void DeficitRoundRobin::schedule(Ptr<QueueItem> p){
+
+  return;
 }
 
 } // namespace ns3
--- deficit-round-robin/model/deficit-round-robin.h
+++ deficit-round-robin/model/deficit-round-robin.h
@@ -6,79 +6,48 @@
 #include "ns3/packet.h"
 #include "ns3/queue.h"
 #include "ns3/nstime.h"
+#include "ns3/base-class.h"
+#include "ns3/destination-portnumber.h"
 
 namespace ns3 {
 
 class TraceContainer;
 
-class DeficitRoundRobin: public Queue {
+class DeficitRoundRobin: public BaseClass {
 public:
 	static TypeId GetTypeId(void);
 
 	DeficitRoundRobin();
-
 	~DeficitRoundRobin();
 
-	void SetMode(DeficitRoundRobin::QueueMode mode);
-	DeficitRoundRobin::QueueMode GetMode(void);
+  	void SetMode(DeficitRoundRobin::QueueMode mode);
+	DeficitRoundRobin::QueueMode GetMode (void);
+
 
 private:
 
-	virtual bool DoEnqueue(Ptr<QueueItem> p);
-	virtual Ptr<QueueItem> DoDequeue(void);
-	virtual Ptr<const QueueItem> DoPeek(void) const;
-
-	// returns 0 for 1st queue
-	// returns 1 for 2nd queue
-	uint16_t Classify(Ptr<Packet> p);
-
-	// returns the queue number to be peeked (for DoPeek() method)
+	bool DoEnqueue(Ptr<QueueItem> p);
+	Ptr<QueueItem> DoDequeue(void);
+	Ptr<const QueueItem> DoPeek(void) const;
+	uint16_t classify(Ptr<QueueItem> p);
 	int32_t GetQueueToBePeeked() const;
+	void schedule(Ptr<QueueItem> p);
 
-	// packet classification port
-	uint32_t m_secondQueuePort;
+	uint32_t quantumSize;
+	uint32_t m_priorityPort;
 
-    // DRR Parameters Qi set for a constant bits
-    // for each queue to served.
-    uint32_t m_quantum_size;
+	uint32_t m_highMaxPackets;
+	uint32_t m_lowMaxPackets;
+
+	uint32_t m_highMaxBytes;
+	uint32_t m_lowMaxBytes;
+
+	bool m_serve_queue2;
+	uint16_t  m_first_dc;
+	uint16_t  m_second_dc;
         
-    // a counter for each unusable Qi.
-    // resets to 0 when the queue is empty.
-    uint32_t m_first_dc;
-    uint32_t m_second_dc;
-    bool m_serve_queue2;
-
-	std::queue<Ptr<Packet> > m_firstQueue;
-	uint32_t m_bytesInFirstQueue;
-	uint32_t m_firstMaxPackets;
-	uint32_t m_firstMaxBytes;
-	uint32_t m_firstPacketsToBeServed;
-	uint32_t m_firstMeanPacketSize;
-	uint32_t m_firstRemainingPacketsToBeServed;
-
-	std::queue<Ptr<Packet> > m_secondQueue;
-	uint32_t m_bytesInSecondQueue;
-	uint32_t m_secondMaxPackets;
-	uint32_t m_secondMaxBytes;
-	uint32_t m_secondPacketsToBeServed;
-	uint32_t m_secondMeanPacketSize;
-	uint32_t m_secondRemainingPacketsToBeServed;
-
-	QueueMode m_mode;
-
-	bool m_isInitialized;
 
 
-	// These variables are temporarily used for showing progression percentage
-	uint64_t m_passedBytes;
-	uint8_t m_20PercentCompleted;
-	uint8_t m_40PercentCompleted;
-	uint8_t m_60PercentCompleted;
-	uint8_t m_80PercentCompleted;
-	uint64_t m_totalExpectedBytes;
-
-	uint64_t max_firstQueue;
-	uint64_t max_secondQueue;
 };
 
 } // namespace ns3
